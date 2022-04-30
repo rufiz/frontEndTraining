55be2e063556deeb61477d2d0611a40a42b45dfa
Rufiz Mardan <rufiz.mardan@gmail.com>
2022-04-30 10:21:07 +0200

    jquery intro

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-$("h1").css("color", "red");
+let makeYellowButton = $(".make-yellow");
+let makeBlackButton = $(".make-black");
+
+function makeYellow(event) {
+    $("h1").addClass("big-title");
+}
+
+function makeBlack(event) {
+    $("h1").removeClass("big-title");
+}
+
+$("button.make-yellow").on("click", makeYellow);
+$("button.make-black").on("click", makeBlack);
